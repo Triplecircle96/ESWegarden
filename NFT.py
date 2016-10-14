965f6d5d505193566586e9512c86610917a05204
smnitro555 <smnitro555@gmail.com>
2016-10-14 18:02:33 -0400

    building out NFT

--- NFT.py
+++ NFT.py
@@ -1,1 +1,9 @@
+import RPi.GPIO as GPIO
+
 class NFT:
+    def __init__(self, motorPin, sensorPin):
+        self.motorPin = motorPin
+        self.SensorPin = sensorPin
+
+    def instantiateSystem(self):
+        
