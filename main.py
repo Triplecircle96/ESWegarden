68bd760dd4f588fac080c3e29b11eba9a759716d
smnitro555 <smnitro555@gmail.com>
2016-10-16 11:15:04 -0400

    No Need for ConfigSectionMap Method, commented for now. Will delete in future

--- main.py
+++ main.py
@@ -1,30 +1,31 @@
 # ESW Egarden - An Effort for Hydroponics to be Automated with a Raspberry-Pi
 
 import ConfigParser
-import RPi.GPIO as GPIO
 
 Config = ConfigParser.ConfigParser()
 Config.read('config.ini')
 systems = []
 
-def ConfigSectionMap(section):
-    dict1 = {}
-    options = Config.options(section)
-    for option in options:
-        try:
-            dict1[option] = Config.get(section, option)
-            if dict1[option] == -1:
-                DebugPrint("skip: %s" % option)
-        except:
-            print("exception on %s!" % option)
-            dict1[option] = None
-    return dict1
+# # Method that parses through config file
+# def ConfigSectionMap(section):
+#     dict1 = {}
+#     options = Config.options(section)
+#     for option in options:
+#         try:
+#             dict1[option] = Config.get(section, option)
+#             if dict1[option] == -1:
+#                 DebugPrint("skip: %s" % option)
+#         except:
+#             print("exception on %s!" % option)
+#             dict1[option] = None
+#     return dict1
 
 numSystems = len(Config.sections())
 
+# Reading through the different systems that are in the Config.ini file
 for i in range(numSystems):
     print "Looking at System %d" % (i + 1)
-    sectionString = 'System' + str(i + 1);
+    sectionString = 'System' + str(i + 1)
     try:
         type = Config.get(sectionString,'Type')
         print "System Type is: %s" % (type)  # TO Fix
@@ -39,17 +40,29 @@ for i in range(numSystems):
 
     # Starting the systems based of System Type
     if (type == 'NFT'):
-        x = NFT(pumpPin,waterLevelPin)
-        systems.append(x)
+        print 'Creating NFT System'
+        # x = NFT(pumpPin,waterLevelPin)
+        # systems.append(x)
     elif (type == 'Drip'):
         # Drip System has unique parameters for it, so parsing for the more information
+        print 'Creating Drip System'
         try:
             onTime = Config.get(sectionString, 'onTime')
             print "System will be On This Much Time per Hour (minutes): %d" % (int(onTime))  # TO Fix
         except:
             print 'Bad Naming of Drip System Timer in Config File, Continuing Haphazardly'
+        # x = Drip(pumpPin, waterLevelPin, onTime)
+        # systems.append(x)
     elif type == 'EbbNFlow':
-
+        # EbbNFlow System has unique parameters for it, so parsing for the more information
+        print 'Creating EbbNFlow System'
+        try:
+            onTime = Config.get(sectionString, 'onTime')
+            print "System will be On This Much Time per Hour (minutes): %d" % (int(onTime))  # TO Fix
+        except:
+            print 'Bad Naming of Drip System Timer in Config File, Continuing Haphazardly'
+        # x = EbbNFlow(pumpPin, waterLevelPin, onTime)
+        # systems.append(x)
     else:
         print 'System type did not Match known types. Check Spelling of System Type'
 
