89c7794712bc2709f42c38ad77131020cef420d6
smnitro555 <smnitro555@gmail.com>
2016-10-28 15:01:49 -0400

    Float Switch with Interrupts

--- systemSensors/Float_Switch.py
+++ systemSensors/Float_Switch.py
@@ -3,14 +3,14 @@ import RPi.GPIO as GPIO
 class Float_Switch(Base_Sensor):
     def __init__(self, sensorPin):
         self.sensorPin
+        GPIO.setup(self.sensorPin, GPIO.IN, pull_up_down=GPIO.PUD)
 
+    def waterLevelLow(self):
+        # Send Turn off Command
 
+    def waterLevelHigh(self):
+        # Send Turn on Command
 
-    def checkWaterContinous(self):
-        while(true):
-            if (self.checkLevel):
+    GPIO.add_event_detect(self.sensorPin, GPIO.FALLING, callback=waterLevelHigh, bouncetime=50)
 
-            else:
-
-
-    def checkLevel(self):
+    GPIO.add_event_detect(self.sensorPin, GPIO.RISING, callback=waterLevelLow, bouncetime=50)
