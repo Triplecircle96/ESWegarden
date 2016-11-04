7b79bbee546a21b94433bef24fb9ced93632644a
smnitro555 <smnitro555@gmail.com>
2016-11-04 15:17:04 -0400

    Incorporating Timer into EbbNFlow

--- systemTypes/drip.py
+++ systemTypes/drip.py
@@ -1,6 +1,7 @@
 import RPi.GPIO as GPIO
 import datetime
 import threading
+from threading import Timer
 from eGardenPackage.systemSensors import *
 
 class drip(system):
@@ -10,27 +11,36 @@ class drip(system):
         self.onTime = onTime
         self.startTime = datetime.datetime.now().time().isoformat()
         self.alive = True
+        # Variables for Timer
+        self._timer = None
+        self.is_running = False
         # Setup GPIO Pin Mode
         GPIO.setup(self.motorPin, GPIO.OUT)
         self.sensor = Float_Switch(self.SensorPin)
 
     def runSystem(self):
-        if self.alive:
             # Turn on the Motor
             GPIO.output(self.motorPin,1)
             # Instantiate Sensor
-        else:
-            GPIO.output(self.motorPin,0)
+
+            # Timer Function to Turn Off
+            if self.is_running:
+
 
     def reactivateSystem(self):
         # Turn On Motor
         self.alive = True
         self.runSystem()
+        # Timer Function
+        if not self.is_running:
+                self._timer = Timer(self.onTime,runSystem)
+                self._timer.start()
+                self.is_running = True;
 
     def deactivateSystem(self):
         # Turn Off Motor
-        self.alive = False
-        self.runSystem()
+        GPIO.output(self.motorPin, 0)
+        self.is_running = False
 
     def diagnostic(self):
         # Prints system information for user
