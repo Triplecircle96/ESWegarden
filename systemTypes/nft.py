8bc382a7bb11d33d828558be274f3709c7292ca3
smnitro555 <smnitro555@gmail.com>
2017-01-20 14:13:31 -0500

    Adding a delay before float switch is registering (sleep not delay)

--- systemTypes/nft.py
+++ systemTypes/nft.py
@@ -24,7 +24,7 @@ class NFT(system.system):
         if (self.debug):
             print("Instantiating NFT System")
         self.runSystem()
-        time.delay(1000)
+        time.sleep(1000)
         while True:
             if not self.sensor.event1.isSet():
                 self.sensor.event1.wait()
